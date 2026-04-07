0f622499d6972a47c669a94350702427f4f47515
Aaron Pham <contact@aarnphm.xyz>
2026-04-07 10:18:25 -0400

    fix: forced dynamic when fetching trpc
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- packages/dashboard/app/api/trpc/[trpc]/route.ts
+++ packages/dashboard/app/api/trpc/[trpc]/route.ts
@@ -2,6 +2,8 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 import { appRouter } from '@/server/router'
 import { createContext } from '@/server/trpc'
 
+export const dynamic = 'force-dynamic'
+
 const handler = (req: Request) =>
   fetchRequestHandler({ endpoint: '/api/trpc', req, router: appRouter, createContext })
 
